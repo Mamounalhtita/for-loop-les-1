02518d8694d34bd5a765d1329f5ae11fea81e0b9
Mamounalhtita <302734233@student.rocmondriaan.nl>
2023-10-06 00:02:29 +0200

    LES 1

---
+++ main.js
@@ -0,0 +1,46 @@
+//opdr 1
+for (let i = 0; i <= 9; i++){
+    console.log(i);
+}
+console.log("Einde programma");
+
+//opdr 2
+for (let e = 0; e <= 20; e++){
+    console.log(e);
+}
+console.log("opdr 2");
+
+//opdr 3
+for (let f = 2; f >= 20; f++) {
+    console.log(f);
+  }
+  
+  console.log("opdr 3");
+
+  //opdr 4
+  for (let r = 10; r >= 0; r--){
+    console.log(r);
+  }
+  console.log("opdr 4");
+
+  //opdr 5
+  for (let d = 40; d >= 4; d--){
+    console.log(d);
+  }
+  console.log("opdr 5");
+
+  //opdr 6
+
+  for (let k = 0; k < 8; k++) {
+    console.log("Ik ontwikkel apps");
+  }
+
+  //opdr 7
+  for (let l = 0; l <= 12; l++) {
+    console.log("Dit is loop: " + l);
+    if (l === 12) {
+      break;
+    }
+  }
+
+  //opdr 8 en 9 vond ik ze moeilijk.
